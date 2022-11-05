7038da72c82b97bd15b5b5ec77a0a1cfdd71830a
JICaddick <james_caddick16@hotmail.com>
2022-11-04 22:46:19 -0500

    There's a bug somewhere on this commit. Possible to do with GraphiQL, possible on product.js.

---
+++ components/ProductPageContent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ProductPageContent({ product }) {
+  return (
+      <div>
+        <h1>{product.title}</h1>
+    </div>
+  )
+}
--- pages/products/[product].js
+++ pages/products/[product].js
@@ -1,21 +1,36 @@
 import { getAllProducts } from "../../lib/shopify"
+import { getAllProducts, getProduct } from "../../lib/shopify"
+
 
 export default function ProductPage({ product }) {
-  return <div></div>
+  return <div>
+    <ProductPageContent product={product} />
+  </div>
 }
 
 export async function getStaticPaths() {
   const products = await getAllProducts()
 
   const paths = products.map((item) => {
-    const handle = String(item.node.handle)
+    const product = String(item.node.handle)
 
     return {
-      params: { handle },
+      params: { product }
     }
   })
+
   return {
     paths,
-    fallback: false,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const product = await getProduct(params.product)
+
+  return {
+    props: {
+      product
+    }
   }
 }
