847c7470d9650a71a6e5a84e1ac7f163dece3e67
JICaddick <james_caddick16@hotmail.com>
2022-11-22 13:35:17 -0500

    hero component done.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import { getProductsInCollection } from "../lib/shopify"
 import ProductList from "../components/ProductList"
+import Hero from "../components/Hero"
 
 export default function Home({ products }) {
   return (
-    <div className="text-3xl">
+    <div className="">
+      <Hero />
       <ProductList products={ products } />
    </div>
   ) 
